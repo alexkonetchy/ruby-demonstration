e67459f0a56bd5c50bb75817924b7e65de9bf817
alexkonetchy <alexkonetchy@live.com>
2015-06-30 17:41:34 -0700

    Update 11_dictionary.rb

--- lib/11_dictionary.rb
+++ lib/11_dictionary.rb
@@ -1,3 +1,51 @@
 class Dictionary
-  # TODO: your code goes here!
+  def initialize
+    @entries = {}
+  end
+  
+  #Method: entries
+  #Author: Alex Konetchy
+  #Returns the entries instance variable
+  def entries
+    @entries
+  end
+  
+  #Method: add
+  #Author: Alex Konetchy
+  #Adds an entry. If a string converts to a hash first
+  def add(entry)
+    entry = {entry => nil} if entry.class == String   #Converts to hash if string
+    entries.merge!(entry)
+  end
+  
+  #Method: keywords
+  #Author: Alex Konetchy
+  #Sorts the keys in the entries hash alphabetically
+  def keywords
+    entries.keys.sort
+  end
+  
+  #Method: include?
+  #Author: Alex Konetchy
+  #Checks to see if the entries hash has a key
+  def include?(key)
+    entries.has_key?(key)
+  end
+  
+  #Method: find
+  #Author: Alex Konetchy
+  #Selects all the entries that match the given regex and the entries key
+  def find(keyword)
+    entries.select { |key, value| key =~ /#{keyword}/ }
+  end
+  
+  #Method: printable
+  #Author: Alex Konetchy
+  #Prints out a readble version of the entries hash
+  def printable 
+    contents = []
+    keywords.each { |word| contents << "[#{word}] \"#{entries[word]}\"" }
+    
+    contents.join("\n")
+  end
 end
