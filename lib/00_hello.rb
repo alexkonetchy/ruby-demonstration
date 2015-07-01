4b70307bc3bfeda8567206344577920d903abbd6
alexkonetchy <alexkonetchy@live.com>
2015-06-30 17:36:08 -0700

    Update 00_hello.rb

---
+++ lib/00_hello.rb
@@ -0,0 +1,13 @@
+#Method: hello
+#Author: Alex Konetchy
+#Returns "Hello!" string
+def hello
+    "Hello!"
+end
+
+#Method: greet
+#Author: Alex Konetchy
+#Returns personalized greeting
+def greet(name)
+    "Hello, #{name}!"
+end
